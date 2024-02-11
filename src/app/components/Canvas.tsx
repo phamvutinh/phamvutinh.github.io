12d85d0d863ab95d69b793eef54ae2d9aec8d7cc
phamvu.tinh <phamvu.tinh99@gmail.com>
2024-02-11 19:15:32 +0700

    fix: error with types

--- src/app/components/Canvas.tsx
+++ src/app/components/Canvas.tsx
@@ -1,26 +1,34 @@
 'use client';
 
-import { MeshDistortMaterial, Shadow, useGLTF } from '@react-three/drei';
+import {
+  MeshDistortMaterial,
+  Shadow,
+  ShadowType,
+  useGLTF
+} from '@react-three/drei';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { useRef } from 'react';
 import * as THREE from 'three';
 
 function Model(props: any) {
-  const group = useRef();
-  const shadow = useRef();
+  const group = useRef<THREE.Group>();
+  const shadow = useRef<THREE.Object3D>(); // Add type assertion here
+
   const { nodes } = useGLTF('/geo.min.glb', true);
   useFrame(({ clock }) => {
     const t = (1 + Math.sin(clock.getElapsedTime() * 1.5)) / 2;
     if (!group.current || !shadow.current) return;
-    group.current.position.y = t / 3;
-    shadow.current.scale.y = shadow.current.scale.z = 1 + t;
-    shadow.current.scale.x = (1 + t) * 1.25;
+    (group.current as THREE.Group).position.y = t / 3;
+    (shadow.current as THREE.Object3D).scale.y = (
+      shadow.current as THREE.Object3D
+    ).scale.z = 1 + t; // Fix the problem by adding type assertion
+    (shadow.current as THREE.Object3D).scale.x = (1 + t) * 1.25; // Fix the problem by adding type assertion
     group.current.rotation.x = group.current.rotation.z += 0.005;
   });
 
   return (
     <group {...props} dispose={null}>
-      <group ref={group}>
+      <group ref={group as React.RefObject<THREE.Group>}>
         <mesh
           geometry={(nodes.geo as THREE.Mesh).geometry}
           castShadow
@@ -40,7 +48,7 @@ function Model(props: any) {
         </mesh>
       </group>
       <Shadow
-        ref={shadow}
+        ref={shadow as React.RefObject<ShadowType>}
         opacity={0.3}
         rotation-x={-Math.PI / 2}
         position={[0, -1.51, 0]}
